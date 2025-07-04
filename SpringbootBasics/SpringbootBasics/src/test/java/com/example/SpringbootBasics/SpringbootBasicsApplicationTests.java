33559157ffe50c3fcedb857ccfe7c09e2ef6b26c
dhanushrithiick <dhanushrithiick@gmail.com>
2025-07-04 08:02:19 +0530

    Add files via upload

---
+++ SpringbootBasics/SpringbootBasics/src/test/java/com/example/SpringbootBasics/SpringbootBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootBasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
